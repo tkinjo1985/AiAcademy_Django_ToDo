3902fb993097b2225a76d9e8e5715c7120d6e7c3
Kinjo.Takumi <tkinjo1@gmail.com>
2020-07-01 21:02:21 +0900

    travi create .env

--- config/dev.py
+++ config/dev.py
@@ -3,9 +3,9 @@ from .base import *
 
 DEBUG = True
 
-# env = environ.Env()
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = 'g+_@20^da-#e&y1fxk+^hl^bkp*1!!o)y05bs8cfrv!!5l_v_('
+env = environ.Env()
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'g+_@20^da-#e&y1fxk+^hl^bkp*1!!o)y05bs8cfrv!!5l_v_('
 
 ALLOWED_HOSTS = ['*', 'testserver']
 
