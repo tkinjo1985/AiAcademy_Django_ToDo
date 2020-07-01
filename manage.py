f7021746cd324bf27bb6293b605d23bafbd8f3ba
Kinjo.Takumi <tkinjo1@gmail.com>
2020-07-01 13:57:14 +0900

    deproy test

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
